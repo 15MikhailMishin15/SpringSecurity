28f0c2c5fde533c96df2f2f99b4a198cf2916fb3
Mikhail Mishin <mishin_mihail_98@mail.ru>
2024-08-01 19:21:41 +0300

    commit 5

--- src/main/java/ru/kata/spring/boot_security/demo/init/DbInit.java
+++ src/main/java/ru/kata/spring/boot_security/demo/init/DbInit.java
@@ -2,6 +2,8 @@ package ru.kata.spring.boot_security.demo.init;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
 import ru.kata.spring.boot_security.demo.entity.Role;
@@ -21,11 +23,13 @@ public class DbInit {
 
     private final UserRepository userRepository;
     private final RoleRepository roleRepository;
+    private final PasswordEncoder passwordEncoder;
 
     @Autowired
     public DbInit(UserRepository userRepository, RoleRepository roleRepository) {
         this.userRepository = userRepository;
         this.roleRepository = roleRepository;
+        this.passwordEncoder = new BCryptPasswordEncoder();
     }
 
     @PostConstruct
@@ -42,12 +46,14 @@ public class DbInit {
         // Создание пользователей
         User admin = new User();
         admin.setUsername("admin");
-        admin.setPassword("admin"); // Замените на закодированный пароль
+        admin.setEmail("admin@admin.com");
+        admin.setPassword(passwordEncoder.encode("100"));
         admin.getRoles().add(adminRole);
 
         User user = new User();
         user.setUsername("user");
-        user.setPassword("user"); // Замените на закодированный пароль
+        user.setEmail("user@user.com");
+        user.setPassword(passwordEncoder.encode("100"));
         user.getRoles().add(userRole);
 
         userRepository.save(admin);
